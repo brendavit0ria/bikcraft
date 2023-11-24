f2ebbe266088a6f5efd29292faec3859a7c7418c
Brenda Vitória <brendavitoria0602@gmail.com>
2023-11-24 16:26:09 -0300

    script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
+// ATIVAR LINKS DO MENU 
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
     const url = location.href;
     const href = link.href;
-
     if (url.includes(href)) {
         link.classList.add("ativo");
     }
 }
 
 links.forEach(ativarLink);
+
+// ATIVAR ITEMS DO ORÇAMENTO
+
+const parametros = new URLSearchParams (location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if(elemento) {
+        elemento.checked = true;
+    }
+    console.log(parametro);
+}
+
+parametros.forEach(ativarProduto);
