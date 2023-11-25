d5072b21107aeae79c31ff8abe7a3f663e26cc36
Brenda Vitória <brendavitoria0602@gmail.com>
2023-11-25 11:24:29 -0300

    galeria de bicicleta

--- js/script.js
+++ js/script.js
@@ -43,3 +43,22 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// GALERIA DE BICICLETA
+
+const galeria = document.querySelectorAll(".bicicletas-imagens img");
+const galeriaContainer = document.querySelector(".bicicletas-imagens");
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media =  matchMedia("(min-width: 1000px)").matches;
+    if(media) {
+    galeriaContainer.prepend(img);
+   }
+}
+
+function eventosGaleria(img) {
+    img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
